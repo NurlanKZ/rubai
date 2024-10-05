38c0609fdafab3f82e727777886d2b3ae0769f1c
Nurlan Kudaibergen <nurlan96@gmail.com>
2024-10-05 11:39:48 +0500

    Fixed audio not playing and optimized to not have a separate fetch for audio source.

--- script.js
+++ script.js
@@ -10,20 +10,17 @@ fetch(`data/${dailyIndex}.json`)
             const poemLineElement = document.getElementById(`line-${index+1}`);
             poemLineElement.innerHTML = content;
         });
+        audio.src = `data:audio/mpeg;base64,${data[0].audio}`;
     });
 
 const containerElement = document.getElementById('box');
     containerElement.addEventListener('click', () => {
         if (audio.paused) {
-            fetch(`data/${randomNumber}.json`)
-                .then(response => response.json())
-                .then(data => {
-                    audio.src = `data:audio/mpeg;base64,${data[0].audio}`;
-                    audio.play();
-                    audio.onended = () => {
-                        audio.currentTime = 0;
-                        audio.pause();
-                    };
-                });
+            audio.src = `data:audio/mpeg;base64,${data[0].audio}`;
+            audio.play();
+            audio.onended = () => {
+                audio.currentTime = 0;
+                audio.pause();
+            };
         }
     });
